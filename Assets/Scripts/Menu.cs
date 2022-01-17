8086da832be3faf8e71728c4f45bd33adcedfd44
MelvinV04 <79506720+MelvinV04@users.noreply.github.com>
2022-01-17 15:24:41 +0100

    Spakgrej

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,8 +6,7 @@ public class Menu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);// this can load the scene number 1 (game)//SceneManager.GetActiveScene().buildIndex +1
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void QuitGame()
     {
---
+++ Assets/Scripts/Spak.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spak : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Debug.Log("test");
+        }
+    }
+}
