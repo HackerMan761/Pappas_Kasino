d43b2fb4e2fc4f4005f138d0bac334aa08146589
MelvinV04 <79506720+MelvinV04@users.noreply.github.com>
2022-02-17 14:56:47 +0100

    p

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -8,7 +8,7 @@ public class Restart : MonoBehaviour
 
     public void RestartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
 }
